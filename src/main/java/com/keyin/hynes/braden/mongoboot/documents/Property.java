b14f7c150b6a1245db9ebd1aacf4b618104ce693
agent-indigo <bradenhynes@gmail.com>
2026-07-22 14:53:29 -0230

    Modified field nullability

--- src/main/java/com/keyin/hynes/braden/mongoboot/documents/Property.java
+++ src/main/java/com/keyin/hynes/braden/mongoboot/documents/Property.java
@@ -7,16 +7,16 @@ import com.keyin.hynes.braden.mongoboot.fields.PropertyContact;
 import com.keyin.hynes.braden.mongoboot.fields.PropertyLocation;
 import com.keyin.hynes.braden.mongoboot.fields.PropertyRates;
 import com.mongodb.lang.NonNull;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.ToString;
+@AllArgsConstructor
 @Data
 @Document(collection = "properties")
 @EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
-@RequiredArgsConstructor
 @ToString(callSuper = true)
 public final class Property extends DocumentBase {
   @NonNull
@@ -37,6 +37,7 @@ public final class Property extends DocumentBase {
   private Integer square_feet;
   @NonNull
   private List<String> amenities;
+  @NonNull
   private PropertyRates rates;
   @NonNull
   private PropertyContact seller_info;
@@ -45,5 +46,5 @@ public final class Property extends DocumentBase {
   @NonNull
   private List<String> imageIds;
   @NonNull
-  private Boolean is_featured;
+  private Boolean is_featured = false;
 }
--- src/main/java/com/keyin/hynes/braden/mongoboot/fields/PropertyRates.java
+++ src/main/java/com/keyin/hynes/braden/mongoboot/fields/PropertyRates.java
@@ -1,8 +1,14 @@
 package com.keyin.hynes.braden.mongoboot.fields;
+import com.mongodb.lang.Nullable;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 @Data
+@NoArgsConstructor
 public final class PropertyRates {
+  @Nullable
   private Integer nightly;
+  @Nullable
   private Integer weekly;
+  @Nullable
   private Integer monthly;
 }
